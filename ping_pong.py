b2dd0711d9dd28201222a3863bc3336d44f8156c
damola <egbebitimi121@gmail.com>
2023-06-03 00:32:51 -0800

    Made some ux chnages, changed all staticmethods to class methods

--- ping_pong.py
+++ ping_pong.py
@@ -1,4 +1,3 @@
-# ping pong
 import pygame
 import random
 from time import time as current_time
@@ -6,13 +5,13 @@ pygame.init()
 pygame.mixer.init()
 pygame.display.set_caption("ping pong")
 win = pygame.display.set_mode((780, 500))
-font = pygame.font.SysFont("algerian", 100)
-text = pygame.font.SysFont("verdana", 20)
-game_score = pygame.font.SysFont("euphemia", 50)
+title_font = pygame.font.SysFont("algerian", 100)
+options_font = pygame.font.SysFont("verdana", 20)
+score_font = pygame.font.SysFont("euphemia", 50)
 white = (255, 255, 255)
 
 
-class player:
+class Player:
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -21,7 +20,9 @@ class player:
         self.x_serve_pos = x
         self.y_serve_pos = y
         self.vel = 10
-        self.serve = False
+        self.score = 0
+        self.area_color = (0, 0, 0)
+        self.time_area_color_changed = 0
 
     def draw(self):
         pygame.draw.rect(win, white, (self.x, self.y, self.width, self.height))
@@ -42,9 +43,14 @@ class player:
                 if keys[pygame.K_s]:
                     self.y += self.vel
 
-    def reset_position(self):
+    def reset(self):
         self.x = self.x_serve_pos
         self.y = self.y_serve_pos
+
+    def area_color_to_black(self):
+        if self.area_color != (0, 0, 0):
+            if current_time() - self.time_area_color_changed >= 0.2:
+                self.area_color = (0, 0, 0)
 
 
 class Computer:
@@ -53,45 +59,54 @@ class Computer:
     width = 15
     height = 120
     vel = 10
+    score = 0
     random_position = None
-    smart_movement = False
     can_move = False
     collision_point_y = None
-    serve = False
     return_to_best_pos = False
+    area_color = (0, 0, 0)
+    time_area_color_changed = 0
 
-    @staticmethod
-    def draw():
-        pygame.draw.rect(win, white, (Computer.x, Computer.y, Computer.width, Computer.height))
+    @classmethod
+    def draw(cls):
+        pygame.draw.rect(win, white, (cls.x, cls.y, cls.width, cls.height))
 
-    @staticmethod
-    def move():
-        if Computer.return_to_best_pos:
-            if Computer.y < 190:
-                Computer.y += Computer.vel
-            elif Computer.y > 190:
-                Computer.y -= Computer.vel
+    @classmethod
+    def move(cls):
+        if cls.return_to_best_pos:
+            if cls.y < 190:
+                cls.y += cls.vel
+            elif cls.y > 190:
+                cls.y -= cls.vel
             else:
-                Computer.return_to_best_pos = False
-        if not (Computer.can_move and Computer.collision_point_y):
+                cls.return_to_best_pos = False
+        if not (cls.can_move and cls.collision_point_y):
             return
-        if not Computer.random_position:
-            Computer.random_position = random.choice([[0, 40],[41, 80],[81, 120]])
-        if not((Computer.y+Computer.random_position[0])<=Computer.collision_point_y and (Computer.y+Computer.random_position[1])>=Computer.collision_point_y):
-            if Computer.collision_point_y < (Computer.y+Computer.random_position[0]):
-                if Computer.y > 0:
-                    Computer.y -= Computer.vel
-            elif Computer.collision_point_y > (Computer.y+Computer.random_position[1]):
-                if Computer.y + 120 < 500:
-                    Computer.y += Computer.vel
+        if not cls.random_position:
+            cls.random_position = random.choice([[0, 40],[41, 80],[81, 120]])
+
+        if not((cls.y+cls.random_position[0])<=cls.collision_point_y and (cls.y+cls.random_position[1])>=cls.collision_point_y):
+            if cls.collision_point_y < (cls.y+cls.random_position[0]):
+                if cls.y > 0:
+                    cls.y -= cls.vel
+            elif cls.collision_point_y > (cls.y+cls.random_position[1]):
+                if cls.y + cls.height < 500:
+                    cls.y += cls.vel
         else:
-            Computer.can_move = False
+            cls.can_move = False
         
-    @staticmethod       
-    def reset_position():
-        Computer.x = 35
-        Computer.y = 190
-        Computer.random_position = None
+    @classmethod      
+    def reset(cls):
+        cls.y = 190
+        cls.random_position = None
+        cls.can_move = True
+        cls.collision_point_y = None
+
+    @classmethod
+    def area_color_to_black(cls):
+        if cls.area_color != (0, 0, 0):
+            if current_time() - cls.time_area_color_changed >= 0.2:
+                cls.area_color = (0, 0, 0)
        
 
 class Ball:
@@ -100,77 +115,60 @@ class Ball:
     radius = 10
     vel_x = 0
     vel_y = 0
-    vel = 15
-    hit_top_wall = False
-    hit_bottom_wall = False
 
-    @staticmethod
-    def draw():
-        pygame.draw.circle(win, (100, 255, 255), (Ball.x, Ball.y), Ball.radius)
+    @classmethod
+    def draw(cls):
+        pygame.draw.circle(win, (100, 255, 255), (cls.x, cls.y), cls.radius)
 
-    @staticmethod
-    def move():
-        Ball.x += Ball.vel_x
-        Ball.y += Ball.vel_y
+    @classmethod
+    def move(cls):
+        cls.x += cls.vel_x
+        cls.y += cls.vel_y
             
-    @staticmethod
-    def collision_with_walls_movement():
-        if Ball.y - Ball.radius == 0:
+    @classmethod
+    def collision_with_walls_movement(cls):
+        if cls.y - cls.radius == 0:
             pygame.mixer.Sound("bounce.mp3").play()
-            Ball.vel_y = Ball.vel
+            cls.vel_y = 15
 
-        if Ball.y + Ball.radius == 500:
+        elif cls.y + cls.radius == 500:
             pygame.mixer.Sound("bounce.mp3").play()
-            Ball.vel_y = -Ball.vel
+            cls.vel_y = -15
             
-
-        if Ball.vel_x < 0:
-            if Ball.x <= 285:
+        if cls.vel_x < 0:
+            if cls.x <= 285:
                 Computer.can_move = True
 
-            elif Ball.vel_y == 0 and Ball.x <= 360:
-                Computer.can_move = True
-
-        if Ball.x + Ball.radius <= 0:
-            GameState.update_score(False)
-        elif Ball.x + Ball.radius >= 780:
-            GameState.update_score(True)
+        if cls.x + cls.radius <= 0:
+            GameState.update_score(player1)
+        elif cls.x + cls.radius >= 780:
+            GameState.update_score(player2)
                 
-    @staticmethod
-    def collision_with(player):
-        if not (Ball.x == 60 or Ball.x == 720):
-            return
-        if not (Ball.y >= player.y and Ball.y <= player.y+120):
-            return
-
-        if Ball.x == 60 and player == player2:
-            Ball.vel_x = Ball.vel
-        elif Ball.x == 720  and player == player1:
-            Ball.vel_x = -Ball.vel
+    @classmethod
+    def check_collision_with(cls, player, new_vel):
+        if (cls.y >= player.y and cls.y <= player.y+40):
+            cls.vel_y = -15
+        elif (cls.y >= player.y+41 and cls.y <= player.y+80):
+            cls.vel_y = 0
+        elif (cls.y >= player.y+81 and cls.y <= player.y+120):
+            cls.vel_y = 15
         else:
             return
+        cls.vel_x = new_vel
 
-        if (Ball.y >= player.y and Ball.y <= player.y+40):
-            Ball.vel_y = -Ball.vel
-        elif (Ball.y >= player.y+41 and Ball.y <= player.y+80):
-            Ball.vel_y = 0
-        elif (Ball.y >= player.y+81 and Ball.y <= player.y+120):
-            Ball.vel_y = Ball.vel
-
-        # return
         if player == player1:
-            if Ball.vel_y < 0:
-                if Ball.y < 180:
-                    Computer.collision_point_y = 300 + Ball.y
+            if cls.vel_y < 0:
+                if cls.y < 180:
+                    Computer.collision_point_y = 300 + cls.y
                 else:
-                    Computer.collision_point_y = 680 - Ball.y
-            elif Ball.vel_y > 0:
-                if Ball.y > 320:
-                   Computer.collision_point_y = Ball.y - 300  
+                    Computer.collision_point_y = 680 - cls.y
+            elif cls.vel_y > 0:
+                if cls.y > 320:
+                   Computer.collision_point_y = cls.y - 300 
                 else:
-                    Computer.collision_point_y = 320-Ball.y
+                    Computer.collision_point_y = 320 - cls.y
             else:
-                Computer.collision_point_y = Ball.y
+                Computer.collision_point_y = cls.y
         else:
             Computer.collision_point_y = None
             Computer.return_to_best_pos = random.choice([True, False])
@@ -183,59 +181,67 @@ class GameState():
     start = False
     multiplayer = True
     player_to_serve = None
-    left_score = 0
-    right_score = 0
     time_started = 0
 
-    @staticmethod
-    def display_game_menu():
+    @classmethod
+    def display_game_menu(cls):
         win.fill((0, 0, 0))
-        win.blit(font.render("PING PONG", True, white), (150, 50))
-        win.blit(text.render("VS Computer", True, white), (200, 310))
-        win.blit(text.render("MULTIPLAYER", True, white), (400, 310))
-        pygame.draw.rect(win, white, (GameState.menu_x, GameState.menu_y, 180, 50), 2)
+        win.blit(title_font.render("PING PONG", True, white), (150, 50))
+        win.blit(options_font.render("VS Computer", True, white), (200, 310))
+        win.blit(options_font.render("MULTIPLAYER", True, white), (400, 310))
+        pygame.draw.rect(win, white, (cls.menu_x, cls.menu_y, 180, 50), 2)
 
-    @staticmethod
-    def highlight_option(move_right):
+    @classmethod
+    def highlight_option(cls, move_right):
         if move_right:
-            GameState.menu_x += 200
+            cls.menu_x += 200
         else:
-            GameState.menu_x -= 200
+            cls.menu_x -= 200
 
-        if GameState.menu_x > 380:
-            GameState.menu_x = 180
-        elif GameState.menu_x < 180:
-            GameState.menu_x = 380
+        if cls.menu_x > 380:
+            cls.menu_x = 180
+        elif cls.menu_x < 180:
+            cls.menu_x = 380
 
-    @staticmethod
-    def select_option():
-        GameState.start = True
-        GameState.player_to_serve = player1
-        if GameState.menu_x == 180:
-            GameState.multiplayer = False
+    @classmethod
+    def select_option(cls):
+        cls.start = True
+        cls.player_to_serve = player1
+        if cls.menu_x == 180:
+            cls.multiplayer = False
             return Computer
-        if GameState.menu_x == 380:
-            GameState.multiplayer = True
-            return player(35, 190)
+        if cls.menu_x == 380:
+            cls.multiplayer = True
+            return Player(35, 190)
 
-    @staticmethod
-    def update_score(right):
-        if right:
-            GameState.right_score += 1
-            GameState.player_to_serve = player1
+    @classmethod
+    def update_score(cls, player):
+        player.score += 1
+        if player == player1:
+            cls.player_to_serve = player2
+            player2.area_color = (70, 0, 0)
+            player2.time_area_color_changed = current_time()
         else:
-            GameState.left_score += 1
-            GameState.player_to_serve = player2
-            player2.can_move = True
-            GameState.time_started = current_time()
-        win.fill((225, 0, 0))
+            cls.player_to_serve = player1
+            player1.area_color = (70, 0, 0)
+            player1.time_area_color_changed = current_time()
+
+        if cls.player_to_serve == player2 and player2 == Computer:
+            cls.time_started = current_time()
+
         Ball.vel_x = 0
         Ball.vel_y = 0
-        player1.reset_position()
-        player2.reset_position()
+        player1.reset()
+        player2.reset()
+
+    @classmethod
+    def update_serving_player(cls):
+        cls.player_to_serve.area_color = (0, 50, 0)
+        cls.player_to_serve.time_area_color_changed = current_time()
+        cls.player_to_serve = None
 
 
-player1 = player(730, 190)
+player1 = Player(730, 190)
 player2 = None
 run = True
 
@@ -263,18 +269,21 @@ while run:
             Ball.y = 250
             Computer.collision_point_y = Ball.y
             if Computer.can_move == False: # computer is in proper ball range
-                GameState.player_to_serve = None
-        elif GameState.player_to_serve == player1:
+                GameState.update_serving_player()
+
+        elif type(GameState.player_to_serve) == Player:
             Ball.x = 720
             Ball.y = 250
             if keys[pygame.K_SPACE] and (Ball.y >= player1.y and Ball.y <= player1.y+120):
-                GameState.player_to_serve = None
+                GameState.update_serving_player()
 
         win.fill((0, 0, 0))
-        pygame.draw.rect(win, (0, 0, 0), (0, 0, 388, 500))
-        win.blit(game_score.render(str(GameState.left_score), True, white), (310, 5))
-        pygame.draw.rect(win, (0, 0, 0), (393, 0, 393, 500))
-        win.blit(game_score.render(str(GameState.right_score), True, white), (450, 5))
+        player2.area_color_to_black()
+        pygame.draw.rect(win, player2.area_color, (0, 0, 398, 500))
+        win.blit(score_font.render(str(player2.score), True, white), (310, 5))
+        player1.area_color_to_black()
+        pygame.draw.rect(win, player1.area_color, (403, 0, 393, 500))
+        win.blit(score_font.render(str(player1.score), True, white), (450, 5))
         for num in range(5, 495, 55):
             pygame.draw.rect(win, white, (398, num, 5, 50))
 
@@ -283,14 +292,16 @@ while run:
         Ball.draw()
         player1.move()
         if GameState.player_to_serve == player2:
-            if (current_time() - GameState.time_started) >= 0.5:
+            if (current_time() - GameState.time_started) >= 0.6:
                 player2.move()
         else:
             player2.move()
         Ball.move()
         if not GameState.player_to_serve:
-            Ball.collision_with(player1)
-            Ball.collision_with(player2)
+            if Ball.x == 720:
+                Ball.check_collision_with(player1, -15)
+            elif Ball.x == 60:
+                Ball.check_collision_with(player2, 15)
         Ball.collision_with_walls_movement()
    
     pygame.display.update()
